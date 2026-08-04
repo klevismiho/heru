82d89ca1c45893883264961f5a85e24025e15a11
Klevis Miho <klevisi@gmail.com>
2026-08-04 08:19:19 +0200

    Update logos-grid block to be carousel

--- assets/build/blocks/logo-grid/render.php
+++ assets/build/blocks/logo-grid/render.php
@@ -11,25 +11,27 @@ $wrapper_attributes = get_block_wrapper_attributes();
 
 <section <?php echo $wrapper_attributes; ?>>
 
-	<h2 class="section-title"><?php echo esc_html( $heading ); ?></h2>
+	<h2 class="section-title"><?php echo esc_html($heading); ?></h2>
 
-	<div class="logo-grid__logos">
-		<?php foreach ( $logos as $logo ) : ?>
-			<div class="logo-grid__item">
-				<?php if ( ! empty( $logo['url'] ) ) : ?>
-					<a href="<?php echo esc_url( $logo['url'] ); ?>">
-						<img src="<?php echo esc_url( $logo['src'] ); ?>" alt="<?php echo esc_attr( $logo['alt'] ?? '' ); ?>" />
-					</a>
-				<?php else : ?>
-					<img src="<?php echo esc_url( $logo['src'] ); ?>" alt="<?php echo esc_attr( $logo['alt'] ?? '' ); ?>" />
-				<?php endif; ?>
-			</div>
-		<?php endforeach; ?>
+	<div class="logo-grid__viewport">
+		<div class="logo-grid__logos">
+			<?php foreach ($logos as $logo) : ?>
+				<div class="logo-grid__item">
+					<?php if (! empty($logo['url'])) : ?>
+						<a href="<?php echo esc_url($logo['url']); ?>">
+							<img src="<?php echo esc_url($logo['src']); ?>" alt="<?php echo esc_attr($logo['alt'] ?? ''); ?>" />
+						</a>
+					<?php else : ?>
+						<img src="<?php echo esc_url($logo['src']); ?>" alt="<?php echo esc_attr($logo['alt'] ?? ''); ?>" />
+					<?php endif; ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
 	</div>
 
 	<div class="wp-block-button">
-		<a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( $button_url ); ?>">
-			<?php echo esc_html( $button_text ); ?>
+		<a class="wp-block-button__link wp-element-button" href="<?php echo esc_url($button_url); ?>">
+			<?php echo esc_html($button_text); ?>
 		</a>
 	</div>
 
--- assets/build/blocks/logo-grid/view.asset.php
+++ assets/build/blocks/logo-grid/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8323aad51afd9ab12451');
+<?php return array('dependencies' => array(), 'version' => '8a509b9dee97cdfb7a40');
